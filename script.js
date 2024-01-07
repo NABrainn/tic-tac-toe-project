038a6c52a0c5c6e456b44947aa770a93f0d1fca8
NA brain <michal2002.60@gmail.com>
2024-01-07 18:40:55 +0100

    add draw check

--- script.js
+++ script.js
@@ -21,7 +21,6 @@ function Gameboard() {
                         gameBoard[i][j] = game.nextToken();
                     }
                     else {
-                        console.log('bruh');
                         game.switchTurn();
                     }
                 }
@@ -29,54 +28,63 @@ function Gameboard() {
             }
         }
     }
-    const displayBoard = () => console.log(gameBoard)
     const checkWinner = () => {
         const isTrue = (element) => element === game.nextToken();
+        //check draw
+        let drawIncrement = 0;
+        const isDraw = (element) => element[drawIncrement] !== 0;
+        const checkDraw = board.getBoard()
+        if(checkDraw.every(isDraw)) {
+            displayControl.announceDraw();
+            eventControl.removeTokenListener();
+        }
+
         //check rows
         let row = 0;
         for(let i=0; i<3; i++){
             if(board.getBoard()[row].every(isTrue)){
-                game.announceWinner();
+                displayControl.announceWinner();
                 eventControl.removeTokenListener();
             }
             row++;
         }
 
-        // //check cols
+        //check cols
         let col = 0;
         for(let i=0; i<3; i++) {
             const column = board.getBoard().map(element => element[col]);
             if(column.every(isTrue)) {
-                game.announceWinner();
+                displayControl.announceWinner();
                 eventControl.removeTokenListener();
             }
             col++;
         }
         
-        // //check diag back
+        //check diag back
         let diagIncrement = 0;
         const diagBack = board.getBoard().map((element) => element[diagIncrement++]);
         if(diagBack.every(isTrue)){
-            game.announceWinner();
+            displayControl.announceWinner();
             eventControl.removeTokenListener();
-        } 
+            
+        }
         
         //check diag forward
         let diagDecrement = 2;
         const diagForward = board.getBoard().map((element) => element[diagDecrement--]);
         if(diagForward.every(isTrue)) {
-            game.announceWinner();
+            displayControl.announceWinner();
             eventControl.removeTokenListener();
-        } 
+        }
     }
-    return { getBoard, setBoard, dropToken, displayBoard, checkWinner }
+    return { getBoard, setBoard, dropToken, checkWinner }
 }
 
 function Player(name, token, nextTurn) { 
     const play = (cellNumber) => {
-        board.dropToken(cellNumber);
-        board.checkWinner();
         game.switchTurn();
+        board.dropToken(cellNumber);
+        board.checkWinner();    
     }
     
     return { name, token, nextTurn, play }
@@ -84,10 +92,7 @@ function Player(name, token, nextTurn) {
 
 function Game() {
     const playerOne = Player('this guy', 'o', true);
-    const playerTwo = Player('that guy', 'x', false);
-    //add default Next token to display
-    
-        
+    const playerTwo = Player('that guy', 'x', false);  
     const startup = () => {
         eventControl.addTokenToDisplay();
     }
@@ -109,8 +114,6 @@ function Game() {
         return playerTwo.token;
     }
 
-    
-
     const playTurn = (cellNumber) => {
         if(playerOne.nextTurn === true) {
             game.getPlayerOne().play(Number(cellNumber));
@@ -119,30 +122,24 @@ function Game() {
             game.getPlayerTwo().play(Number(cellNumber));
         }
     }
-    const announceWinner = () => {
-        const selectResult = document.getElementById('result');
-        // const selectToken = document.getElementById('token');
-        getPlayers().map(player => {
-            if(player.token === game.nextToken()) {
-                selectResult.innerText = `The winner is: ${player.name}`;
-            }
-        })
-    }
 
     const cleanup = () => {
         board.setBoard();
         displayControl.nextToken();
         displayControl.setCells('');
         eventControl.addTokenToDisplay();
-        displayControl.result()
+        displayControl.result();
     }
     
-    return { startup, getPlayers, getPlayerOne, getPlayerTwo, switchTurn, nextToken, announceWinner, playTurn, cleanup }
+    return { startup, getPlayers, getPlayerOne, getPlayerTwo, switchTurn, nextToken, playTurn, cleanup }
 }
 
 function displayController() {
+    const selectToken = document.getElementById('token');
+    const selectResult = document.getElementById('result');
+    const selectPlayAgainBtn = document.getElementById('play-again');
+    const selectFooter = document.getElementById('footer');
     const nextToken = () => {
-        const selectToken = document.getElementById('token');
         game.getPlayers().map(player => {
             if(player.token !== game.nextToken()) {
                 selectToken.innerText = `Next token: ${player.token}`;    
@@ -150,28 +147,43 @@ function displayController() {
         })
     }
 
-
     const setCells = (value) => {
         const selectCells = document.querySelectorAll('.cell');
         selectCells.forEach(cell => cell.innerText = value)
     }
 
-    const result = () => {
-        const selectResult = document.getElementById('result');
+    const result = () => {      
         selectResult.innerText = 'Game in progress';
     }
 
-    return { nextToken, setCells, result }
+    const announceWinner = () => {
+        game.getPlayers().map(player => {
+            if(player.token === game.nextToken()) {
+                selectResult.innerText = `The winner is: ${player.name}`;
+                selectToken.style.display = 'none';
+                selectPlayAgainBtn.style.display = 'block';
+                eventControl.addPlayAgainBtnListener();
+            }
+        })
+    }
+
+    const announceDraw = () => {
+        selectResult.innerText = `Draw`;
+        selectToken.style.display = 'none';
+        selectPlayAgainBtn.style.display = 'block';
+    }
+
+    return { nextToken, setCells, result, announceWinner, announceDraw, selectToken, selectFooter, selectPlayAgainBtn }
 }
 
 function eventController() {
     const selectCells = document.querySelectorAll('.cell');
     const listenerFunc = function() {
         game.playTurn(this.id);
-            if(this.innerText === '' || this.innerText === game.nextToken()){
-                this.innerText = game.nextToken();
-                displayControl.nextToken(); 
-            }
+        if(this.innerText === '' || this.innerText === game.nextToken()){
+            this.innerText = game.nextToken();
+            displayControl.nextToken(); 
+        }
     }
     const addTokenToDisplay = () => {    
         selectCells.forEach(cell => {
@@ -184,24 +196,38 @@ function eventController() {
         })
     }
 
-    const addRestartBtnListener = () => {
-        const selectBtn = document.getElementById('start');
+    const addPlayBtnListener = () => {
+        const selectBtn = document.getElementById('play');
+        
         selectBtn.addEventListener('click', () => {
             game.cleanup();
+            displayControl.selectFooter.style.display = 'block';
+            selectBtn.style.display = 'none';
         })
     }
-    
-    return { addTokenToDisplay, removeTokenListener, addRestartBtnListener }
+
+    const addPlayAgainBtnListener = () => {
+        
+        displayControl.selectPlayAgainBtn.addEventListener('click', () => {
+            game.cleanup();
+            displayControl.selectFooter.style.display = 'block';
+            displayControl.selectToken.style.display = 'block';
+            displayControl.selectPlayAgainBtn.style.display = 'none';
+        })
+    } 
+    return { addTokenToDisplay, removeTokenListener, addPlayBtnListener, addPlayAgainBtnListener }
 }
 
 const board = Gameboard();
 const game = Game();
-board.setBoard()
+board.setBoard();
 const eventControl = eventController();
 const displayControl = displayController();
 displayControl.nextToken();
-eventControl.addTokenToDisplay();
-eventControl.addRestartBtnListener()
+displayControl.result()
+
+eventControl.addPlayBtnListener();
+
 
 
 
